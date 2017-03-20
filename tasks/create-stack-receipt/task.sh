bd1e6429d57cdff9bd0a39afedea9c06fd8c0826
Alex Harford <alex.harford@hpe.com>
2017-03-20 14:17:28 -0700

    Fix receipt location

--- tasks/create-stack-receipt/task.sh
+++ tasks/create-stack-receipt/task.sh
@@ -6,8 +6,8 @@ set -o pipefail
 
 set -x
 
-mkdir -p stack-artifacts
-receipt_filename="stack-artifacts/opensuse-cf-stack_receipt-$(cat version/number)"
-echo "Rootfs SHA256SUM: $(sha256sum rootfs.tar | awk '{print $1}')" > "stack-artifacts/${receipt_filename}"
-echo "" > "stack-artifacts/${receipt_filename}"
-cat "stack-artifacts/opensuse-cf-stack_rpm_qa.out" >> "stack-artifacts/${receipt_filename}"
+mkdir -p receipt-artifacts
+receipt_filename="receipt-artifacts/opensuse-cf-stack_receipt-$(cat version/number)"
+echo "Rootfs SHA256SUM: $(sha256sum rootfs.tar | awk '{print $1}')" > "${receipt_filename}"
+echo "" > "${receipt_filename}"
+cat "stack-artifacts/opensuse-cf-stack_rpm_qa.out" >> "${receipt_filename}"
