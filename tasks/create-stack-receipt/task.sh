d632b7ebd07b12d6e0a0241550f96fb2289150a1
Alex Harford <alex.harford@hpe.com>
2017-03-20 13:49:47 -0700

    Fix version input location

--- tasks/create-stack-receipt/task.sh
+++ tasks/create-stack-receipt/task.sh
@@ -7,7 +7,7 @@ set -o pipefail
 set -x
 
 mkdir -p stack-artifacts
-receipt_filename="stack-artifacts/opensuse-cf-stack_receipt-$(cat ../version/number)"
+receipt_filename="stack-artifacts/opensuse-cf-stack_receipt-$(cat version/number)"
 echo "Rootfs SHA256SUM: $(sha256sum rootfs.tar | awk '{print $1}')" > "stack-artifacts/${receipt_filename}"
 echo "" > "stack-artifacts/${receipt_filename}"
 cat "stack-artifacts/opensuse-cf-stack_rpm_qa.out" >> "stack-artifacts/${receipt_filename}"
