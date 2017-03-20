eb648e07d43cfc774aaec54ba0d7b5dc3edbbde0
Alex Harford <alex.harford@hpe.com>
2017-03-20 14:36:40 -0700

    Fix path to stack rootfs.tar

--- tasks/create-stack-receipt/task.sh
+++ tasks/create-stack-receipt/task.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 set -o errexit
 set -o nounset
@@ -8,6 +8,6 @@ set -x
 
 mkdir -p receipt-artifacts
 receipt_filename="receipt-artifacts/opensuse-cf-stack_receipt-$(cat version/number)"
-echo "Rootfs SHA256SUM: $(sha256sum rootfs.tar | awk '{print $1}')" > "${receipt_filename}"
+echo "Rootfs SHA256SUM: $(sha256sum opensuse-cf-stacks-latest-image/rootfs.tar | awk '{print $1}')" > "${receipt_filename}"
 echo "" > "${receipt_filename}"
 cat "stack-artifacts/opensuse-cf-stack_rpm_qa.out" >> "${receipt_filename}"
