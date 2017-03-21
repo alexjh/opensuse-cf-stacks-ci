be499ad840a0f6ad444a2c437e348314daecc2f1
Alex Harford <alex.harford@hpe.com>
2017-03-21 11:04:54 -0700

    Fix path to build scripts

--- tasks/build-binary/task.sh
+++ tasks/build-binary/task.sh
@@ -8,6 +8,6 @@ set -x
 
 mkdir -p build-binary
 pushd build-binary
-  /opensuse-cf-binary-builder/bin/binary-builder --name="${NAME}" --version="${VERSION}" --sha256="${SHA256}"
-  /opensuse-cf-fs2/bin/sha1stamp ./*.tar.gz
+  ../opensuse-cf-binary-builder/bin/binary-builder --name="${NAME}" --version="${VERSION}" --sha256="${SHA256}"
+  ../opensuse-cf-fs2/bin/sha1stamp ./*.tar.gz
 popd
