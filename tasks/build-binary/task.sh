c03b34c64d8248d6892359aa85c6ea6ada50ff44
Alex Harford <alex.harford@hpe.com>
2017-03-21 12:57:09 -0700

    binary-builder expects to run from its root

--- tasks/build-binary/task.sh
+++ tasks/build-binary/task.sh
@@ -7,7 +7,8 @@ set -o pipefail
 set -x
 
 mkdir -p build-binary
-pushd build-binary
-  ../opensuse-cf-binary-builder/bin/binary-builder --name="${NAME}" --version="${VERSION}" --sha256="${SHA256}"
+pushd opensuse-cf-binary-builder
+  ./bin/binary-builder --name="${NAME}" --version="${VERSION}" --sha256="${SHA256}"
   ../opensuse-cf-fs2/bin/sha1stamp ./*.tar.gz
+  mv ./*.tar.gz ../build-binary/
 popd
