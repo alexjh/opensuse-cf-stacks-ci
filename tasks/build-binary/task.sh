a05a53f63db5bacbd8f526a4f80b4afb97e5946b
Alex Harford <alex.harford@hpe.com>
2017-03-21 10:52:30 -0700

    Add binary-builder pipeline
    This is hardcoded for node 7.7.3 for testing

---
+++ tasks/build-binary/task.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+set -x
+
+mkdir -p build-binary
+pushd build-binary
+  /opensuse-cf-binary-builder/bin/binary-builder --name="${NAME}" --version="${VERSION}" --sha256="${SHA256}"
+  /opensuse-cf-fs2/bin/sha1stamp ./*.tar.gz
+popd
